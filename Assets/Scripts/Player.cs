48564f873ea06434196762546b464a49b1a0d9c2
Amit Sheokand <amit.sheokand@tooli.qa>
2024-02-16 09:25:45 +0530

    lifecycle example

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int count;
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("count " + count);
+        count++;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hello from Start");
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
